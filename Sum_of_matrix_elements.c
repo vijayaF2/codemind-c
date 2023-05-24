5dd187f35102087667f3cac6764c931c72ad9e7f
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-05-24 16:36:49 +0530

    #include<stdio.h> int main() {     int a[100][100],i,j,sum=0,n,m;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {          sum=sum+a[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],i,j,sum=0,n,m;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+         sum=sum+a[i][j];    
+        }
+    }
+    printf("%d",sum);
+}
