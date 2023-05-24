af99ede4b78281916dc8eb185bd729ba1d32bff1
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-05-24 19:48:06 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c,d,t,k=1,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<=n-1;i++)     {         c=0;         t=a[i];         if(a[i]==0)         {             c=1;         }         while(t!=0)         {             c++;             d=t%10;             t=t/10;         }         if(k<c)         {             k=c;         }     }     for(i=0;i<=n-1;i++)     {         c=0;         t=a[i];         if(a[i]==0)         {             c=1;         }         while(t!=0)         {             c++;             d=t%10;             t=t/10;         }         if(c==k)         {             printf("%d ",a[i]);         }
        }
        //printf("%d",s);
    }

---
+++ maximum_digit_numbers_count.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],c,d,t,k=1,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=n-1;i++)
+    {
+        c=0;
+        t=a[i];
+        if(a[i]==0)
+        {
+            c=1;
+        }        
+        while(t!=0)
+        {
+            c++;
+            d=t%10;
+            t=t/10;
+        }
+        if(k<c)
+        {
+            k=c;
+        }
+    }
+    for(i=0;i<=n-1;i++)
+    {
+        c=0;
+        t=a[i];
+        if(a[i]==0)
+        {
+            c=1;
+        }        
+        while(t!=0)
+        {
+            c++;
+            d=t%10;
+            t=t/10;
+        }
+        if(c==k)
+        {
+            printf("%d ",a[i]);
+        }
+        
+    }
+    //printf("%d",s);
+}
