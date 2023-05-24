805aaaf7c57774a9f1a2633e022bd11ac3db1ab2
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-05-24 19:36:16 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a];     for(int i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int flag=0;     int d=arr[0]-arr[1];     if(d<0)     {         for(int i=0;i<a-1;i+=2)         {             if(arr[i+1]>arr[i])             {                 flag=1;             }             else             {                 flag=0;                 break;             }         }     }     else     {         for(int i=0;i<a-1;i+=2)         {             if(arr[i+1]<arr[i])             {                 flag=2;             }             else             {                 flag=0;                 break;             }         }     }     if(flag==1)     {         int c=0;         for(int i=1;i<a-1;i+=2)         {             if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])             {                 c++;             }         }         printf("%d",c);     }     else if(flag==2)     {         int c=0;         for(int i=1;i<a-1;i+=2)         {             if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])             {                 c++;             }         }         printf("%d",c);     }     else     {         printf("-1");     } }

---
+++ wave_count.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int flag=0;
+    int d=arr[0]-arr[1];
+    if(d<0)
+    {
+        for(int i=0;i<a-1;i+=2)
+        {
+            if(arr[i+1]>arr[i])
+            {
+                flag=1;
+            }
+            else
+            {
+                flag=0;
+                break;
+            }
+        }
+    }
+    else
+    {
+        for(int i=0;i<a-1;i+=2)
+        {
+            if(arr[i+1]<arr[i])
+            {
+                flag=2;
+            }
+            else
+            {
+                flag=0;
+                break;
+            }
+        }
+    }
+    if(flag==1)
+    {
+        int c=0;
+        for(int i=1;i<a-1;i+=2)
+        {
+            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
+            {
+                c++;
+            }
+        }
+        printf("%d",c);
+    }
+    else if(flag==2)
+    {
+        int c=0;
+        for(int i=1;i<a-1;i+=2)
+        {
+            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
+            {
+                c++;
+            }
+        }
+        printf("%d",c);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
