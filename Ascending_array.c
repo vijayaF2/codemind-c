2e1ea7923f871846d439284c10a4284367449e4c
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-05-24 19:39:47 +0530

    #include<stdio.h> int main() {     int n,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n-1;i++)     {         if(a[i]>=a[i+1])         {             printf("no");             return 0;         }     }     printf("yes"); }

---
+++ Ascending_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]>=a[i+1])
+        {
+            printf("no");
+            return 0;
+        }
+    }
+    printf("yes");
+}
